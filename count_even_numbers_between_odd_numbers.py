c539a293f447d3602eee6779428e5b6464ebc114
Divyaratnam <132640690+Divyaratnam@users.noreply.github.com>
2023-06-03 17:14:08 +0530

    def odd_e_odd(arr):     c=0     for i in range(1,len(arr)-1):         if arr[i]%2==0 and arr[i-1]%2!=0 and arr[i+1]%2!=0:          c+=1     return c n=int(input()) arr=list(map(int,input().split())) print(odd_e_odd(arr))

---
+++ count_even_numbers_between_odd_numbers.py
@@ -0,0 +1,9 @@
+def odd_e_odd(arr):
+    c=0
+    for i in range(1,len(arr)-1):
+        if arr[i]%2==0 and arr[i-1]%2!=0 and arr[i+1]%2!=0:
+         c+=1
+    return c
+n=int(input())
+arr=list(map(int,input().split()))
+print(odd_e_odd(arr))
